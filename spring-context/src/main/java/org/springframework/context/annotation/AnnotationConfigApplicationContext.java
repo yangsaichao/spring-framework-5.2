5765d27efa83f30c00a41bde92870847b8a01881
shadow <18274279727@139.com>
2021-03-14 23:02:44 +0800

    test coommit

--- spring-context/src/main/java/org/springframework/context/annotation/AnnotationConfigApplicationContext.java
+++ spring-context/src/main/java/org/springframework/context/annotation/AnnotationConfigApplicationContext.java
@@ -59,6 +59,7 @@ public class AnnotationConfigApplicationContext extends GenericApplicationContex
 
 
 	/**
+	 * 构造方法
 	 * Create a new AnnotationConfigApplicationContext that needs to be populated
 	 * through {@link #register} calls and then manually {@linkplain #refresh refreshed}.
 	 */
